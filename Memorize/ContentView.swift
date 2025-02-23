a4e2ecfec0d167dc096d2349d8fe46f76e1a9908
Joqtán Mármol <joqtanmarmol@gmail.com>
2025-02-22 21:00:36 -0600

    add cards, faceUp state and onTapGesture action

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
         }
+        .foregroundColor(.orange)
         .padding()
+    }
+}
+
+struct CardView: View {
+
+    @State private var isFaceUp: Bool = true
+
+    var body: some View {
+    
+        content
+            .rotation3DEffect(
+                .degrees(isFaceUp ? 0 : 180),
+                axis: (x: 0, y: 1, z: 0)
+            )
+            .onTapGesture {
+                withAnimation(.easeInOut(duration: 0.3)) {
+                    isFaceUp.toggle()
+                }
+            }
+    }
+
+    var frontView: some View {
+        ZStack {
+            BaseCard()
+                .foregroundStyle(.white)
+                .overlay(RoundedRectangle(cornerRadius: 12).stroke(.orange, lineWidth: 3))
+
+            Text("🐲")
+                .font(.largeTitle)
+        }
+    }
+
+    var backView: some View {
+        BaseCard()
+    }
+
+    @ViewBuilder
+    var content: some View {
+        if isFaceUp {
+            frontView
+        } else {
+            backView
+
+        }
+    }
+}
+
+struct BaseCard: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 12)
     }
 }
 
